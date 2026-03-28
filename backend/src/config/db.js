6a742accb8fd151caaf1119dd953ac1b3adc5f2c
nam145127 <van145127@gmail.com>
2026-03-28 21:23:58 +0700

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,15 +1,11 @@
 const { Pool } = require("pg");
-require("dotenv").config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 5432,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false, 
+    rejectUnauthorized: false,
   },
+  family: 4,
 });
 
 module.exports = pool;
